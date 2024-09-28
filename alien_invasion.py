9e1e7b9234f85286378f8fb1e58dba4f723161f0
Anthony Rivers <81377656+rememberanthonyrivers@users.noreply.github.com>
2024-09-28 00:27:50 -0400

    added git ignore file

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,17 +10,20 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
 
-    # lets set the background color
+    # lets set the background color, specified 
+    # before entrering the main loop
     bg_color = (230, 230, 230) 
 
     # start the game loop
     while True:
+
         # watches for keyboard and mouse events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-
+        # redraws the screen during each pass through the loop
+        screen.fill(bg_color)
         # make the most recently drawn screen visible
         pygame.display.flip()
 
